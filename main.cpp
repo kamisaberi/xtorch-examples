d04d0a71585d2957c5f89fdf7b4f770160001193
kamran saberifard <kamisaberi@gmail.com>
2025-04-18 15:43:29 +0330

    simple exmaple finished

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@
 
 
 int main() {
-    // torch::Tensor tensor = torch::eye(3);
-    // std::cout << tensor << endl;
-    // std::cout << "Hello World" << std::endl;
-    // std::string  r = "./test/test1/ali";
+    torch::Tensor tensor = torch::eye(3);
+    std::cout << tensor << endl;
+    std::cout << "Hello World" << std::endl;
+    std::string  r = "./test/test1/ali";
     // torch::ext::data::datasets::UCF101 u1(r);
     // std::cout << "End\n";
 //    torch::ext::data::datasets::CIFAR100 cifar100("/home/kami/Documents/temp/", true , true);
