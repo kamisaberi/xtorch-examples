2cc5a732afcec22b7da3e6d8ddd7084e74de8709
saeed <kamisaberi@gmail.com>
2025-04-20 13:25:43 +0330

    hello world created

--- introduction/hello-world.cpp
+++ introduction/hello-world.cpp
@@ -1,4 +1,15 @@
-//
-// Created by pc on 4/19/2025.
-//
+#include <torch/torch.h>
+#include <iostream>
+#include <xtorch/xtorch.h>
+
+int main() {
+    torch::Tensor tensor = torch::eye(3);
+    std::cout << tensor << endl;
+    std::cout << "Hello World" << std::endl;
+    std::string  r = "./test/test1/ali";
+    auto temp = xt::temp::TestDataset();
+    auto dataset = xt::data::datasets::MNIST("/home/kami/Documents/temp/", DataMode::TRAIN, true);
+
+    return 0;
+}
 
