3104bf4f3339d17104f3950d30c15114381e68a9
kamisaberi <kamisaberi@gmail.com>
2025-06-06 15:07:16 +0330

    some bugs fixed

--- introduction/hello-world.cpp
+++ introduction/hello-world.cpp
@@ -3,12 +3,7 @@
 #include <xtorch/xtorch.h>
 
 int main() {
-    torch::Tensor tensor = torch::eye(3);
-    std::cout << tensor << endl;
-    std::cout << "Hello World" << std::endl;
-    std::string  r = "./test/test1/ali";
-    auto temp = xt::temp::TestDataset();
-    auto dataset = xt::data::datasets::MNIST("/home/kami/Documents/datasets/", DataMode::TRAIN, true);
+
 
     return 0;
 }
