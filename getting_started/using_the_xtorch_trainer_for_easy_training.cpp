be87135cc83e092eb4e62c53fc1f35f5743ce2da
kamisaberi <kamisaberi@gmail.com>
2025-06-25 23:37:06 +0330

    new examples folders created

--- getting_started/using_the_xtorch_trainer_for_easy_training.cpp
+++ getting_started/using_the_xtorch_trainer_for_easy_training.cpp
@@ -1,4 +1,55 @@
-//
-// Created by pc on 4/22/2025.
-//
+#include <iostream>
+#include <torch/torch.h>
+#include <torch/data/datasets.h> // For torch::data::Dataset
+#include <torch/data/example.h>  // For torch::data::Example
+#include <vector>
+#include <string>
+#include <algorithm> // For std::shuffle, std::iota, std::min, std::max
+#include <future>    // Not directly used in V2, but good to keep in mind for other patterns
+#include <optional>   // For torch::optional and std::optional
+#include <chrono>     // For timing in main
+#include <xtorch/xtorch.h>
 
+using namespace std;
+
+
+
+int main()
+{
+    std::cout.precision(10);
+
+    std::vector<std::shared_ptr<xt::Module>> transform_list;
+    transform_list.push_back(std::make_shared<xt::transforms::image::Resize>(std::vector<int64_t>{32, 32}));
+    transform_list.push_back(
+        std::make_shared<xt::transforms::general::Normalize>(std::vector<float>{0.5}, std::vector<float>{0.5}));
+
+    auto compose = std::make_unique<xt::transforms::Compose>(transform_list);
+
+    auto dataset = xt::datasets::MNIST(
+        "/home/kami/Documents/datasets/", xt::datasets::DataMode::TRAIN, false, std::move(compose));
+
+
+
+    xt::dataloaders::ExtendedDataLoader data_loader(dataset, 64, true, 2, /*prefetch_factor=*/2);
+
+    xt::models::LeNet5 model(10);
+    model.to(torch::Device(torch::kCPU));
+    model.train();
+
+    torch::optim::Adam optimizer(model.parameters(), torch::optim::AdamOptions(1e-3));
+
+    auto logger = std::make_shared<xt::LoggingCallback>("[MyTrain]", /*log_every_N_batches=*/20, /*log_time=*/true);
+    xt::Trainer trainer;
+    trainer.set_max_epochs(10).set_optimizer(optimizer)
+           .set_loss_fn([](auto output, auto target)
+           {
+               return torch::nll_loss(output, target);
+           })
+           .add_callback(logger);
+
+    trainer.fit(model, data_loader, &data_loader, torch::Device(torch::kCPU));
+
+
+
+    return 0;
+}
