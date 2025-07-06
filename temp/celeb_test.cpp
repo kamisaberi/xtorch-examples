67036e09a54e3b7e63c66b62703dcba4ffc68764
kamran saberifard <kamisaberi@gmail.com>
2025-07-06 16:00:33 +0330

    time_series_folder.cpp created

---
+++ temp/celeb_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <vector>
+#include <xtorch/xtorch.h>
+#include <torch/script.h>
+#include <torch/torch.h>
+
+namespace fs = std::filesystem;
+
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
