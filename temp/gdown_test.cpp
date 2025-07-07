1dd9630f7a8174426066e170439c744db230cea3
kamran saberifard <kamisaberi@gmail.com>
2025-07-08 03:12:47 +0330

    download_from_gdrive function changed to download_from_google_drive

--- temp/gdown_test.cpp
+++ temp/gdown_test.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
-    xt::utils::download_from_google_drive("0B7EVK8r0v71pblRyaVFSWGxPY0U", "00000000000000000000000000000000" , "/home/kami/temp/list_attr_celeba.txt");
+    // xt::utils::download_from_google_drive("0B7EVK8r0v71pblRyaVFSWGxPY0U", "00000000000000000000000000000000" , "/home/kami/temp/list_attr_celeba.txt");
+    auto dataset = xt::datasets::CelebA("/home/kami/temp" , xt::datasets::DataMode::TRAIN , true);
     return 0;
 }
