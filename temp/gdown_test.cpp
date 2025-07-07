16f71a6692a65c996a20a3669e398f44e72a470e
kamran saberifard <kamisaberi@gmail.com>
2025-07-08 01:20:02 +0330

    download_from_gdrive function changed to download_from_google_drive

---
+++ temp/gdown_test.cpp
@@ -0,0 +1,10 @@
+#include <xtorch/xtorch.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    xt::utils::download_from_google_drive("0B7EVK8r0v71pblRyaVFSWGxPY0U", "00000000000000000000000000000000" , "/home/kami/temp/list_attr_celeba.txt");
+    return 0;
+}
