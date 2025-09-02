56e6ce6e8db3ef00b6f2a3269d22410f2e9976e4
kamran saberifard <kamisaberi@gmail.com>
2025-09-02 03:59:41 +0330

    some bugs fixed

--- computer_vision/image_classification/classifying_handwritten_digits_with_lenet_on_mnist.cpp
+++ computer_vision/image_classification/classifying_handwritten_digits_with_lenet_on_mnist.cpp
@@ -32,101 +32,50 @@ int main()
     model.to(torch::Device(torch::kCPU));
     model.train();
     torch::optim::Adam optimizer(model.parameters(), torch::optim::AdamOptions(1e-3));
-    auto start_time = std::chrono::steady_clock::now();
-    for (int i = 1; i <= epochs; i++)
-    {
-        int btc = 0;
-        for (auto& batch_data : data_loader)
-        {
-            btc++;
-            auto epoch_start = std::chrono::steady_clock::now();
-
-            torch::Tensor data = batch_data.first;
-            torch::Tensor target = batch_data.second;
-
-            // if (btc % 20 == 0)
-            // {
-            //     auto t = std::chrono::steady_clock::now();
-            //     auto d = t - epoch_start;
-            //     auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(d);
-            //     cout << "COPY: " << btc << " DIFF:" << d.count() << endl;
-            // }
-
-            auto output_any = model.forward({data});
-            // if (btc % 20 == 0)
-            // {
-            //     auto t = std::chrono::steady_clock::now();
-            //     auto d = t - epoch_start;
-            //     auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(d);
-            //     cout << "FORWARD: " << btc << " DIFF:" << d.count() << endl;
-            // }
-
-
-            auto output = std::any_cast<torch::Tensor>(output_any);
-            torch::Tensor loss = torch::nll_loss(output, target);
-            // if (btc % 20 == 0)
-            // {
-            //     auto t = std::chrono::steady_clock::now();
-            //     auto d = t - epoch_start;
-            //     auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(d);
-            //     cout << "LOSS: " << btc << " DIFF:" << d.count() << endl;
-            // }
-
-            loss.backward();
-            // if (btc % 20 == 0)
-            // {
-            //     auto t = std::chrono::steady_clock::now();
-            //     auto d = t - epoch_start;
-            //     auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(d);
-            //     cout << "BACKWARD: " << btc << " DIFF:" << d.count() << endl;
-            // }
-
-
-            optimizer.zero_grad();
-            // if (btc % 20 == 0)
-            // {
-            //     auto t = std::chrono::steady_clock::now();
-            //     auto d = t - epoch_start;
-            //     auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(d);
-            //     cout << "ZERO GRAD: " << btc << " DIFF:" << d.count() << endl;
-            // }
-
-            optimizer.step();
-            // if (btc % 20 == 0)
-            // {
-            //     auto t = std::chrono::steady_clock::now();
-            //     auto d = t - epoch_start;
-            //     auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(d);
-            //     cout << "STEP: " << btc << " DIFF:" << d.count() << endl;
-            // }
-
-            if (btc % 20 == 0)
-            {
-                cout << "Batch: " << btc << " Loss:" << loss.item() << endl;
-            }
-        }
-    }
-    auto end_time = std::chrono::steady_clock::now();
-    auto duration = end_time - start_time;
-    auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
-    std::cout << "Total loop duration: " << duration_ms.count() << " milliseconds." << std::endl;
-
-    // auto logger = std::make_shared<xt::LoggingCallback>("[MyTrain]", /*log_every_N_batches=*/20, /*log_time=*/true);
-    // xt::Trainer trainer;
-    //
-    // trainer.set_max_epochs(1).set_optimizer(optimizer)
-    //        .set_loss_fn([](const auto& output, const auto& target)
-    //        {
-    //            return torch::nll_loss(output, target);
-    //        })
-    //        .add_callback(logger);
     // auto start_time = std::chrono::steady_clock::now();
-    // trainer.fit(model, data_loader, &data_loader, torch::Device(torch::kCPU));
+    // for (int i = 1; i <= epochs; i++)
+    // {
+    //     int btc = 0;
+    //     for (auto& batch_data : data_loader)
+    //     {
+    //         btc++;
+    //         auto epoch_start = std::chrono::steady_clock::now();
     //
+    //         torch::Tensor data = batch_data.first;
+    //         torch::Tensor target = batch_data.second;
+    //         auto output_any = model.forward({data});
+    //         auto output = std::any_cast<torch::Tensor>(output_any);
+    //         torch::Tensor loss = torch::nll_loss(output, target);
+    //         loss.backward();
+    //         optimizer.zero_grad();
+    //         optimizer.step();
+    //         if (btc % 20 == 0)
+    //         {
+    //             cout << "Batch: " << btc << " Loss:" << loss.item() << endl;
+    //         }
+    //     }
+    // }
     // auto end_time = std::chrono::steady_clock::now();
     // auto duration = end_time - start_time;
     // auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
     // std::cout << "Total loop duration: " << duration_ms.count() << " milliseconds." << std::endl;
+
+    auto logger = std::make_shared<xt::LoggingCallback>("[MyTrain]", /*log_every_N_batches=*/20, /*log_time=*/true);
+    xt::Trainer trainer;
+
+    trainer.set_max_epochs(1).set_optimizer(optimizer)
+           .set_loss_fn([](const auto& output, const auto& target)
+           {
+               return torch::nll_loss(output, target);
+           })
+           .add_callback(logger);
+    auto start_time = std::chrono::steady_clock::now();
+    trainer.fit(model, data_loader, &data_loader, torch::Device(torch::kCPU));
+
+    auto end_time = std::chrono::steady_clock::now();
+    auto duration = end_time - start_time;
+    auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    std::cout << "Total loop duration: " << duration_ms.count() << " milliseconds." << std::endl;
 
     return 0;
 }
